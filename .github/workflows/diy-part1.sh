8621177e3d22e0130162f88dc907b5d24468970e
carbonjiao <carbonjiao@gmail.com>
2021-07-05 18:55:28 +0800

    Create diy-part1.sh

---
+++ .github/workflows/diy-part1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# version
+TZ='Asia/Shanghai' echo "r`date '+%y%m%d'`-`git log -n 1 --format='%H'|cut -c-8`" > version
